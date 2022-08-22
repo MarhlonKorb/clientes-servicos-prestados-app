6be2522f7c5f67210dd43b8e9c3a64af8e67c744
Marhlon Korb <marhlonkorb@hotmail.com>
2022-08-21 23:19:02 -0300

    Criacao dos eventbind e databinds
    Foram criados conceitos de data e event binds para entender o funcionamento da manipulação de informações vindas de objetos de uma classe para o frontend e também foi implementado um método que realiza o submit do formulário para enviar informações ao backend(a conexão com a API ainda não foi feita).

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,24 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Cliente } from '../cliente';
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
-  styleUrls: ['./clientes-form.component.css'],
+  styleUrls: ['./clientes-form.component.css']
 })
 export class ClientesFormComponent implements OnInit {
   cliente: Cliente;
 
-<<<<<<< Updated upstream
-  constructor() { }
-=======
   constructor() {
     this.cliente = new Cliente();
   }
 
   ngOnInit(): void {}
->>>>>>> Stashed changes
+
 
   onSubmit() {
     console.log(this.cliente);
   }
+
 }
