42b12345cc9262cee28b7601b256535bc6b8b10e
Marhlon Korb <marhlonkorb@hotmail.com>
2022-09-04 22:43:41 -0300

    formatacao

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule} from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component';
 import { ClientesListaComponent } from './clientes-lista/clientes-lista.component';
 
 @NgModule({
-  declarations: [ClientesFormComponent,
-     ClientesListaComponent],
-  imports: [CommonModule,
-     ClientesRoutingModule,
-      FormsModule ],
-  exports: [ClientesFormComponent,
-     ClientesListaComponent],
+  declarations: [ClientesFormComponent, ClientesListaComponent],
+  imports: [CommonModule, ClientesRoutingModule, FormsModule],
+  exports: [ClientesFormComponent, ClientesListaComponent],
 })
 export class ClientesModule {}
